91480b0abce08c25ce30d00f0e1f4d80744e58f3
Akshat <82810411+Afterdie@users.noreply.github.com>
2024-05-22 02:19:17 +0530

    added vercel db

--- database.js
+++ database.js
@@ -2,11 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.PG_USERNAME,
-  password: process.env.PG_PASS,
-  host: "localhost",
-  port: 5432,
-  database: "testing",
+  connectionString: process.env.POSTGRES_URL,
 });
 
 module.exports = pool;
